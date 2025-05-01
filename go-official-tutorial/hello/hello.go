d669bd554354e8102bcdba085bff26401a9f5182
Bianca de Castro <biancardc24@gmail.com>
2025-05-01 10:28:57 +0200

    Add multiple greetings

--- go-official-tutorial/hello/hello.go
+++ go-official-tutorial/hello/hello.go
@@ -12,15 +12,18 @@ func main() {
 	log.SetPrefix("hello: ") // the log entry prefix and a flag to disable printing
 	log.SetFlags(0)          // the time, source file, and line number.
 
-	//Request a greeting message
-	message, err := greetings.Hello("Gladys")
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
 	// If an error was returned, print it to the console and
 	// exit the program.
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// If no error was returned, print the returned message
-	// to the console.
-	fmt.Println(message)
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
 }
