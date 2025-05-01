57724f054f703daa4845e103f92107ee90db6725
Bianca de Castro <biancardc24@gmail.com>
2025-05-01 11:24:08 +0200

    Update test from hardcode to assert

--- go-official-tutorial/greetings/greetings_test.go
+++ go-official-tutorial/greetings/greetings_test.go
@@ -3,24 +3,29 @@ package greetings
 import (
 	"regexp"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 // TestHelloName calls greetings.Hello with a name, checking
 // for a valid return value.
 func TestHelloName(t *testing.T) {
+	//given
+	assert := assert.New(t)
 	name := "Gladys"
-	want := regexp.MustCompile(`\b` + name + `\b`)
+	//when
 	msg, err := Hello("Gladys")
-	if !want.MatchString(msg) || err != nil {
-		t.Errorf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
-	}
+	//then
+	assert.Empty(err)
+	assert.Regexp(regexp.MustCompile(`\b`+name+`\b`), msg)
 }
 
 // TestHelloEmpty calls greetings.Hello with an empty string,
 // checking for an error.
 func TestHelloEmpty(t *testing.T) {
+	//when
 	msg, err := Hello("")
-	if msg != "" || err == nil {
-		t.Errorf(`Hello("") = %q, %v, want "", error`, msg, err)
-	}
+	//then
+	assert.Empty(t, msg)
+	assert.EqualError(t, err, "empty name")
 }
